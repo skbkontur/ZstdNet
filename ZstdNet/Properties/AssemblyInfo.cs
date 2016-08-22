1711d6d54a00e7b8107a9bf35c4eaeb76a036e32
Alexander Borzunov <borzunov@skbkontur.ru>
2016-08-22 12:53:19 +0500

    Add assembly description

--- ZstdNet/Properties/AssemblyInfo.cs
+++ ZstdNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZstdNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Zstd wrapper for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SKB Kontur")]
 [assembly: AssemblyProduct("ZstdNet")]
