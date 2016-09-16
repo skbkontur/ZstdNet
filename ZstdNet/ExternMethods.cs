ff14b4b3614109736d84f7ad9fb150438e029363
Daniel Lazzari Jr <daniel.lazzari@sony.com>
2016-09-15 17:32:03 -0700

    Specify Cdecl calling convention for calls into zstdlib_x86.dll. Fixes #4

--- ZstdNet/ExternMethods.cs
+++ ZstdNet/ExternMethods.cs
@@ -16,52 +16,52 @@ namespace ZstdNet
 		private const string DllName = "zstdlib_x86.dll";
 #endif
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZDICT_trainFromBuffer(byte[] dictBuffer, size_t dictBufferCapacity, byte[] samplesBuffer, size_t[] samplesSizes, uint nbSamples);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern uint ZDICT_isError(size_t code);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZDICT_getErrorName(size_t code);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZSTD_createCCtx();
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_freeCCtx(IntPtr cctx);
 		
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZSTD_createDCtx();
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_freeDCtx(IntPtr cctx);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_compressCCtx(IntPtr ctx, IntPtr dst, size_t dstCapacity, IntPtr src, size_t srcSize, int compressionLevel);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_decompressDCtx(IntPtr ctx, IntPtr dst, size_t dstCapacity, IntPtr src, size_t srcSize);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZSTD_createCDict(byte[] dict, size_t dictSize, int compressionLevel);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_freeCDict(IntPtr cdict);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_compress_usingCDict(IntPtr cctx, IntPtr dst, size_t dstCapacity, IntPtr src, size_t srcSize, IntPtr cdict);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZSTD_createDDict(byte[] dict, size_t dictSize);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_freeDDict(IntPtr ddict);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_decompress_usingDDict(IntPtr dctx, IntPtr dst, size_t dstCapacity, IntPtr src, size_t srcSize, IntPtr ddict);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern ulong ZSTD_getDecompressedSize(IntPtr src, size_t srcSize);
 
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern int ZSTD_maxCLevel();
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern size_t ZSTD_compressBound(size_t srcSize);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern uint ZSTD_isError(size_t code);
-		[DllImport(DllName)]
+		[DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr ZSTD_getErrorName(size_t code);
 	}
 }
