b10e282e9c1a188b0ae5acb615898b94383e6ec6
AlgorithmsAreCool <AlgorithmsAreCool@gmail.com>
2018-01-12 16:44:06 -0600

    Add some basic performance testing

---
+++ ZstdNet.Benchmarks/Main.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ZstdNet.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<CompressionBenchmarks>();
+        }
+    }
+}
