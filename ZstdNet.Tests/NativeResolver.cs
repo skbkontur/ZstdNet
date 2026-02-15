313e5a868afe17250b41faa22281762f6204d6b4
Dmitriy Titarenko <dscheg@users.noreply.github.com>
2026-02-15 17:12:37 +0500

    NUnit -> MSTest

--- ZstdNet.Tests/NativeResolver.cs
+++ ZstdNet.Tests/NativeResolver.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: DoNotParallelize]
 
 namespace ZstdNet.Tests;
 
-[SetUpFixture]
+[TestClass]
 // Native dependencies are not added to the deps.json file via ProjectReference
 // https://github.com/dotnet/sdk/issues/10575
 public class NativeResolver
 {
-	[OneTimeSetUp]
-	public static void SetLibZstdResolver()
+	[AssemblyInitialize]
+	public static void SetLibZstdResolver(TestContext _)
 	{
 		var ext = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "dll" : RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "dylib" : "so";
 		NativeLibrary.SetDllImportResolver(typeof(DictBuilder).Assembly, (name, _, _) => name == "libzstd"
